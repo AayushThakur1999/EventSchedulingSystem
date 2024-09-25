d2c6bcf672d2fa6338938b698d4412860b5ad4f1
Aayush Thakur <ashcold21@gmail.com>
2024-09-25 17:26:05 +0530

    add logic for connection to mongodb atlas

--- EventSchedulingSystem-Backend/src/index.ts
+++ EventSchedulingSystem-Backend/src/index.ts
@@ -1,15 +1,31 @@
+import connectDB from "./db";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+connectDB();
+
+/* import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
 import express from "express";
+import { log } from "console";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send(
-    `<h2 style="text-align: center; padding-top: 2rem; color: #37cdbe;">EventSchedulingSystem</h2>`
-  );
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`);
-});
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      log("ERR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      log(`App is listening on port ${process.env.PORT || 3000}...`);
+    });
+  } catch (error) {
+    console.error("ErroR:", error);
+    throw error;
+  }
+})(); */
