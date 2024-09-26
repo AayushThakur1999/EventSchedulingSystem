d6892e7d66066f88113bb564c99cea3dd2365f1c
Aayush Thakur <ashcold21@gmail.com>
2024-09-26 12:15:12 +0530

    configure express, cors and cookie-parser in app.ts

---
+++ EventSchedulingSystem-Backend/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+// used for parsing incoming JSON payloads in request bodies(req.body)
+app.use(express.json({ limit: "16kb" }));
+
+// Parses incoming URL-encoded payloads in request bodies(req.body)
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+// Used to perform CRUD operations on cookies present on the user's browser
+app.use(cookieParser());
+
+export { app };
--- EventSchedulingSystem-Backend/src/index.ts
+++ EventSchedulingSystem-Backend/src/index.ts
@@ -1,9 +1,25 @@
+import { log } from "console";
 import connectDB from "./db";
 import dotenv from "dotenv";
+import { app } from "./app";
 
 dotenv.config();
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      log("ERR:-", err);
+      throw err;
+    });
+    app.listen(port, () => {
+      log(`Server is running at port: ${port}...`);
+    });
+  })
+  .catch((err) => {
+    log("MongoDb connection failed !!!", err);
+  });
 
 /* import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
