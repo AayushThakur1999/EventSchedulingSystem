891c18f7284f55cde38e868930850e4ab269c52c
Aayush Thakur <ashcold21@gmail.com>
2025-01-15 19:04:43 +0530

    added proxy for both local development (vite.config.ts) and production deployment (netlify.toml).

--- EventSchedulingSystem-Frontend/vite.config.ts
+++ EventSchedulingSystem-Frontend/vite.config.ts
@@ -4,8 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    // Used during local development to proxy requests from the frontend to the backend
     proxy: {
-      "^/(users|availability|attendee)": "http://localhost:8000/api/v1", // match each route with same baseURL
+      // "^/(users|availability|attendee)": "http://localhost:8000/api/v1", // match each route with same baseURL
+      "^/(users|availability|attendee)": "https://eventschedulingsystem.onrender.com/api/v1", // match each route with same baseURL
       /* Below is the alternate approach */
       // "/users": "http://localhost:8000/api/v1",
       // "/availability": "http://localhost:8000/api/v1",
